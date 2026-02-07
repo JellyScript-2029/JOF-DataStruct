c5c74dc02dc5431ad99415f1e1dbef4d67559876
jalyanIT-2029 <jhelianne.figuerres@lorma.edu>
2026-02-07 20:04:37 +0800

    sawakas

--- Clothing.java
+++ Clothing.java
@@ -5,24 +5,29 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Clothing {
+    public static  Scanner input = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         File output = new File("Clothing.txt"); // File var = new File ("File name"); assigns a text to a var
         FileWriter fw = new FileWriter(output); // FileWriter var = new FileWriter(filevar); to write to the text file
-        Scanner input = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int registration = 0;
         char strResponse = 'y';
 
-        System.out.println("========================================");
-        fw.write("========================================");
-        System.out.println("          WELCOME TO JXY STORE");
-        fw.write("\n          WELCOME TO JXY STORE");
-        System.out.println("========================================");
-        fw.write("\n========================================");
+        System.out.println("""
+                ========================================
+                          WELCOME TO JXY STORE
+                ========================================
+                """);
+        fw.write("""
+                ========================================
+                          WELCOME TO JXY STORE
+                ========================================
+                """);
+    
 
         do {
             System.out.println("\t  PRODUCT REGISTRATION");
-            fw.write("\n\t  PRODUCT REGISTRATION ");
+            fw.write("\t  PRODUCT REGISTRATION ");
             System.out.print("Brand name: ");
             sb.append("\nBrand name: ").append(input.nextLine()).append("\n");
             System.out.print("Product: ");
@@ -35,20 +40,22 @@ public class Clothing {
             sb.append("Price:").append(input.nextLine()).append("\n");
             fw.write(sb.toString());
             registration++;
-            System.out.println("----------------------------------------");
-            fw.write("----------------------------------------");
+
+            System.out.println("========================================");
+            fw.write("========================================");
             System.out.print("\n\tAnother customer? (Y/N): ");
             strResponse = input.nextLine().charAt(0);
             fw.write("\n\tAnother customer? (Y/N): " + strResponse);
             sb.append("\n\tAnother customer? (Y/N): ").append(strResponse).append("\n");
-            System.out.println("\n----------------------------------------");
+            System.out.println("\n========================================");
+            fw.write("\n========================================");
 
         } while (strResponse == 'y' || strResponse == 'Y');
-
-        System.out.println(registration + " items registered");
-        fw.write(registration + " items registered");
-        System.out.println("========================================");
-        fw.write("========================================");
+    
+        System.out.println("\n" + registration + " items registered");
+        fw.write("\n" + registration + " items registered");
+        System.out.println("\n========================================");
+        fw.write("\n========================================");
         fw.close();
 
     }
